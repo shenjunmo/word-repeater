a506d21fd8222f8c4f85acc63773545add5c6baa
shenjunmo <1388868@uath.edu>
2023-03-17 10:06:42 -0600

    Resolves issue #1

--- word-repeater.py
+++ word-repeater.py
@@ -5,9 +5,11 @@ def main():
     word = input("Enter word to repeat: ")
     n = int(input("Enter number of times to repeat: "))
 
-    for i in range(1, n):
+    for i in range(0, n):
         print(word)
 
 
 if __name__ == "__main__":
     main()
+
+
